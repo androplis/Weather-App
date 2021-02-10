1fb88fda24dd2dba25d26e911f6e66c1bf45dfae
Andrew Biddle <andrewbiddlebugs@gmail.com>
2021-02-10 18:53:19 -0500

    Established API connection, stored weather in state

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,27 @@ import Dashboard from './Dashboard';
 class App extends Component {
 
   state = {
-    weather: []
+    weather: {}
   }
 
   componentDidMount()  {
     this.getWeather();
   }
 
-  getWeather = (query="45039", type) => {
-    let url = `https://api.weatherapi.com/v1/current.json?key=cc1d4248f67f410a8bb230040210602&q=${query}`;
+  getWeather = (query="45039", type="current", days="1") => {
+    let url;
+    if (type === "current") {
+      url = `https://api.weatherapi.com/v1/current.json?key=cc1d4248f67f410a8bb230040210602&q=${query}`;
+    }
+    else {
+      url =`https://api.weatherapi.com/v1/forecast.json?key=cc1d4248f67f410a8bb230040210602&q=${query}&days=${days}`;
+    }
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({ weather: data})
+    });
   }
 
   render() {
